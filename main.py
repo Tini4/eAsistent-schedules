508b556ee85f90eb2c7c40a8df5375d741e5a4c2
Tini4 <tilen.jurican@gmail.com>
2021-04-02 14:16:09 +0200

    calling shower thoughts in main and passing it to html

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
 from schedules import init, get_classroom_schedule, get_grade_schedule
 from flask import Flask, render_template
+from shower_thoughts import get_shower_thoughts
 
 app = Flask('app')
 init()
 classroom_schedule = get_classroom_schedule()
 grade_schedule = get_grade_schedule()
+shower_thoughts = get_shower_thoughts()
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html', classroom_schedule=classroom_schedule, grade_schedule=grade_schedule)
+    return render_template('index.html',
+                           classroom_schedule=classroom_schedule,
+                           grade_schedule=grade_schedule,
+                           shower_thoughts=shower_thoughts
+                           )
 
-
-'''
-for day in classroom_schedule:
-    print(f'\n\n{day.name}\n')
-    for hour in day.hours:
-        print()
-        print(hour.grade)
-        print(hour.professor)
-
-print('\n\n\n')
-
-for day in grade_schedule:
-    print(f'\n\n{day.name}\n')
-    for hour in day.hours:
-        print()
-        print(hour.subject)
-        print(hour.professor)
-        print(hour.classroom)
-'''
 
 if __name__ == "__main__":
     app.run(debug=True)

3248f399b97f75a3ab64e2a536556a2c4e7c1535
Tini4 <tilen.jurican@gmail.com>
2021-04-02 19:16:50 +0200

    added schedules, not working properly yet

--- main.py
+++ main.py
@@ -2,21 +2,48 @@ from schedules import init, get_classroom_schedule, get_grade_schedule
 from flask import Flask, render_template
 from shower_thoughts import get_shower_thoughts
 import random
+import schedule
+import time
 
 app = Flask('app')
 # init()
 # classroom_schedule = get_classroom_schedule()
 # grade_schedule = get_grade_schedule()
 shower_thoughts = get_shower_thoughts()
-print(shower_thoughts)
+shower_thought = random.choice(shower_thoughts)
 
 
-@app.route('/', methods=['POST', 'GET'])
+def update_shower_thoughts():
+    shower_thoughts = get_shower_thoughts()
+
+
+def update_schedules():
+    # init()
+    # classroom_schedule = get_classroom_schedule()
+    # grade_schedule = get_grade_schedule()
+    pass
+
+
+def next_thought():
+    shower_thought = random.choice(shower_thoughts)
+
+
+schedule.every(2).hours.at(':00').do(update_shower_thoughts)
+schedule.every(5).minutes.at(':00').do(update_schedules)
+schedule.every(20).seconds.do(next_thought)
+
+
+'''while True:
+    schedule.run_pending()
+    time.sleep(1)'''
+
+
+@app.route('/')  # , methods=['POST', 'GET'])
 def index():
     return render_template('index.html',
                            # classroom_schedule=classroom_schedule,
                            # grade_schedule=grade_schedule,
-                           shower_thought=random.choice(shower_thoughts)
+                           shower_thought=shower_thought
                            )
 
 
